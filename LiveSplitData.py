a06458718cc9e7255740cfd987ac27257f1eee69
electrorayer <electrorayer@gmail.com>
2023-09-02 20:37:08 +0200

    get category, subcategory and splits when importing lss file(s)

---
+++ LiveSplitData.py
@@ -0,0 +1,31 @@
+import xml.etree.ElementTree as ET
+
+
+class LiveSplitData():
+    def __init__(self, path: str):
+        self.set_XML_root(path)
+
+    def set_XML_root(self, path):
+        xtree = ET.parse(path)
+        self.xroot = xtree.getroot()
+
+    def get_subcategory(self) -> str:
+        category = self.xroot.find('CategoryName').text
+        vars = self.xroot.find('Metadata').find('Variables')
+
+        variables = ''
+        for var in vars:
+            if var.text.lower() in ('yes', 'no'):
+                variables += f"{var.attrib.get('name')} "
+            else:
+                variables += f'{var.text} '
+            
+        if variables != '':
+            category = f'{category} ({variables.strip()})'
+
+        return category
+
+    def get_split_names(self) -> list:
+        return [segment.find('Name').text for segment in self.xroot.find('Segments')]
+    
+    
